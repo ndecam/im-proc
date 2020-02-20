2c35407f93b57015f366fc1b29cfbe2bde9e56bf
ndecam <nicolas.decam@etu.u-bordeaux.fr>
2020-02-20 13:21:07 +0100

    Session de merde

--- zoom/filter.c
+++ zoom/filter.c
@@ -29,16 +29,18 @@ double mitchell_netravali(double x){
 }
 
 double h(double x,char* filter){
-  if(strcmp(filter, "box")){
+  printf("Filter is %s|\n",filter);
+  if(strcmp(filter, "box")==0){
+    printf("Box returned %f\n",box(x));
     return box(x);
   }
-  if(strcmp(filter, "tent")){
+  if(strcmp(filter, "tent")==0){
     return tent(x);
   }
-  if(strcmp(filter, "bell")){
+  if(strcmp(filter, "bell")==0){
     return bell(x);
   }
-  if(strcmp(filter, "mitch")){
+  if(strcmp(filter, "mitch")==0){
     return mitchell_netravali(x);
   }
   return 0;
@@ -48,38 +50,65 @@ void process(int factor, char* ims, char* imd,char* filter){
     int cols = pnm_get_width(imageims);
     int rows = pnm_get_height(imageims);
     int domain;
-    if(strcmp(filter, "box")){
+    if(strcmp(filter, "box")==0){
       domain=1;
     }
-    if(strcmp(filter, "tent")){
+    if(strcmp(filter, "tent")==0){
       domain = 2;
     }
-    if(strcmp(filter, "bell")){
+    if(strcmp(filter, "bell")==0){
       domain = 3;
     }
-    if(strcmp(filter, "mitch")){
+    if(strcmp(filter, "mitch")==0){
       domain = 4;
     }
-    pnm imageimd = pnm_new(cols*factor, rows*factor, PnmRawPpm);
-
-    //TO DO:
-    for(int channel;channel <3;channel ++){
+    pnm imageimd = pnm_new(cols*factor, rows, PnmRawPpm);
+    printf("Doing rotation on cols = %d , rows = %d\n",cols,rows);
+    for(int channel=0;channel <3;channel ++){
       for(int i =0;i<rows;i++)
-        for(int j_prime=0;j_prime<cols;j_prime++){
+        for(int j_prime=0;j_prime<cols*factor;j_prime++){
+          printf("J_prime = %d\n",j_prime);
           double j = j_prime/factor;
-          int left = j-(domain/2);
-          int right = j+(domain/2);
+          float left = j-(domain/2);
+          float right = j+(domain/2);
+          if(left<0) left = 0;
+          if(right>cols) right = cols;
           unsigned short S;
+          printf("Left is %f,right is %f\n",left,right);
           for(int k=left;k<right;k++){
-            S = S + pnm_get_component(imageims,i,k,channel)*h(k-j,filter);
+            S = S + (pnm_get_component(imageims,i,k,channel)*h(k-j,filter));
           }
+          printf("New component in %d\n",S);
           pnm_set_component(imageimd,i,j_prime,channel,S);
         }
-
     }
     pnm_save(imageimd,PnmRawPpm,imd);
 
 }
+
+void rotate(char* ims, char* imd){
+  pnm imageims = pnm_load(ims);
+  int cols = pnm_get_width(imageims);
+  int rows = pnm_get_height(imageims);
+  pnm imageimd = pnm_new(rows, cols, PnmRawPpm);
+  for(int channel = 0; channel <3; channel++){
+    for(int i=0; i< rows; i++){
+      for(int j=0; j< cols; j++ ){
+        int newi = cols - i-1;
+        int newj = i;
+        unsigned short value = pnm_get_component(imageims, i, j, channel);
+        pnm_set_component(imageimd,newi,newj,channel,value);
+
+
+      }
+    }
+  }
+
+  pnm_save(imageimd,PnmRawPpm,imd);
+}
+
+
+
 
 void usage(){
     printf("filter <factor> <filter-name> <ims> <imd>");
@@ -95,7 +124,22 @@ main(int argc, char *argv[])
   char* filter = argv[2];
   char* ims = argv[3];
   char* imd = argv[4];
-  process(factor,ims,imd,filter);
+
+  process(factor,ims,"intermediare.ppm",filter);
+  printf("first process done\n");
+
+  rotate("intermediare.ppm","rotate.ppm");
+  printf("rotate done\n");
+
+  process(factor,"rotate.ppm",imd,filter);
+  printf("second process done\n");
+
+  rotate(imd,imd);
+  printf("rotate done\n");
+  rotate(imd,imd);
+  printf("rotate done\n");
+  rotate(imd,imd);
+  printf("rotate done\n");
 
   return EXIT_SUCCESS;
 }
